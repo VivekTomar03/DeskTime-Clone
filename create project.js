d9b29dcc6213d84238bf614bc54f670b2bc6a29d
VivekTomar03 <tomarvivek90viv@gmail.com>
2022-11-12 23:37:58 +0530

    day5 all task done

--- create project.js
+++ create project.js
@@ -79,6 +79,60 @@ function openNav() {
   let acc_data = JSON.parse(localStorage.getItem("login"));
   document.querySelector("#acc").innerText = acc_data
 
+
+  let button = document.querySelector("#createnow")
+  button.addEventListener("click" , function(event){
+    event.preventDefault()
+    let newprojectwin = document.querySelector("#createprojectpop")
+    newprojectwin.style.display="block"
+  })
+
+  
+    let array =JSON.parse(localStorage.getItem("project")) || []
+  let form = document.querySelector("form")
+  let num = 1
+  form.addEventListener("submit" , function(){
+    let obj = {
+        name : form.projectname.value,
+        hours_rate : form.projecthrs.value,
+        total_cost : form.cost.value,
+        total_hours  : form.hours.value,
+        project_num : num++
+    }
+     array.push(obj)
+     localStorage.setItem("project" ,JSON.stringify(array))
+  })
+
+  let projectdata = JSON.parse(localStorage.getItem("project"))
+ 
+  function displayproject(data){
+    data.forEach(function(ele){
+        let div = document.createElement("div")
+
+        let name = document.createElement("h1")
+        name.innerText = ele.name;
+         
+        let showbrn = document.createElement("button")
+        showbrn.innerText = "Show Project"
+        showbrn.addEventListener("click" , function(){
+            window.location="./projectlist.html"
+        })
+        div.append(name , showbrn)
+        document.querySelector("#projectlist").append(div)
+    })
+  }
+  displayproject(projectdata)
+
+  function display(data){
+    console.log(data.length)
+    
+      document.querySelector("#totalproject").innerText = data.length
+      document.querySelector("#month").innerText = data.length
+      document.querySelector("#week").innerText = data.length
+   
+  }
+  display(projectdata)
+
   let logo = document.querySelector("#logo")
   logo.addEventListener("click" , function(){
     alert("You must logout first from profile")
